cf545e3e77849f78a9cbe103901e81dd925ded7a
ahussien <ahussien@kn-it.com>
2023-11-28 16:57:19 +0200

    Initial commit

---
+++ src/test/java/com/main/project/common/commonproject/CommonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.project.common.commonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
